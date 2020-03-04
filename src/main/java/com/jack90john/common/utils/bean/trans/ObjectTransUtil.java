3eb5eb951be83d29e9860daeeac3f613d9c9e000
jack <jack90john@hotmail.com>
2020-03-04 11:56:07 +0800

    实现Serializable接口的类采用深拷贝，增加IgnoreClass注解

--- src/main/java/com/jack90john/common/utils/bean/trans/ObjectTransUtil.java
+++ src/main/java/com/jack90john/common/utils/bean/trans/ObjectTransUtil.java
@@ -1,7 +1,9 @@
 package com.jack90john.common.utils.bean.trans;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
 
+import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -13,7 +15,7 @@ import java.util.stream.Collectors;
  * 对象转换工具
  *
  * @author jack
- * @version 1.2.0
+ * @version 1.3.0
  * @since 1.0.0.RELEASE
  */
 
@@ -23,23 +25,22 @@ public class ObjectTransUtil {
     }
 
     /**
-     * 转换对象方法，待转换对象需要标注{@link BeanTrans}注解
+     * 转换对象方法，待转换对象需要忽略的字段可以通过{@link IgnoreFields}注解标注
      * 待转换对象和转换后对象变量名称如果一样可以默认可以匹配
      * 如果待转换对象和转换后对象变量名不同，则需要通过{@link TransTarget}注解标注
      *
-     * @param k 待转换对象，需要标注{@link BeanTrans}注解
+     * @param k      待转换对象，需要忽略的字段通过{@link IgnoreFields}注解标注
      * @param vClass 结果对象class
-     * @param <K> 待转换对象类型
-     * @param <V> 结果对象类型
+     * @param <K>    待转换对象类型
+     * @param <V>    结果对象类型
      * @return 转换后得到待对象
-     * @throws IllegalAccessException 反射异常
+     * @throws IllegalAccessException    反射异常
      * @throws InvocationTargetException 反射异常
      * @see IgnoreFields 需要忽略字段
      * @see TransTarget 指定目标变量
      * @since 1.0.0.RELEASE
      */
-    public static <K, V> V transBean(K k, Class<V> vClass) throws IllegalAccessException, InvocationTargetException {
-
+    public static <K extends Serializable, V> V transBean(K k, Class<V> vClass) throws IllegalAccessException, InvocationTargetException {
         Class<?> kClass = k.getClass();
         List<Field> fieldList = Arrays.asList(kClass.getDeclaredFields());
         // 检查是否标注BeanTrans注解，已过时
@@ -54,7 +55,7 @@ public class ObjectTransUtil {
             }
         }
         //循环待转换变量，通过反射一一转换得到结果。
-        V v = null;
+        V v;
         try {
             v = vClass.getDeclaredConstructor().newInstance();
         } catch (InstantiationException e) {
@@ -66,25 +67,30 @@ public class ObjectTransUtil {
             String name = field.getName();
             TransTarget transTarget = field.getAnnotation(TransTarget.class);
             String s = transTarget != null ? transTarget.value() : name;
-            Field targetField;
-            try {
-                targetField = vClass.getDeclaredField(s);
-            } catch (NoSuchFieldException e) {
-                continue;
-            }
-            if (field.getType().equals(targetField.getType())) {
-                String sourceMethodName = "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
-                String targetMethodName = "set" + s.substring(0, 1).toUpperCase() + s.substring(1);
-                Method sourceMethod;
-                Method targetMethod;
+            IgnoreClass ignoreClass = field.getAnnotation(IgnoreClass.class);
+            Class<?>[] classes = ignoreClass != null ? ignoreClass.value() : null;
+            if (ArrayUtils.isEmpty(classes) ||
+                    !Arrays.asList(classes).contains(vClass)) {
+                Field targetField;
                 try {
-                    sourceMethod = kClass.getDeclaredMethod(sourceMethodName);
-                    targetMethod = vClass.getDeclaredMethod(targetMethodName, field.getType());
-                } catch (NoSuchMethodException e) {
-                    throw new ObjectTransException("无法获取到对应get/set方法。", e);
+                    targetField = vClass.getDeclaredField(s);
+                } catch (NoSuchFieldException e) {
+                    continue;
                 }
-                Object invoke = sourceMethod.invoke(k);
-                targetMethod.invoke(v, invoke);
+                if (field.getType().equals(targetField.getType())) {
+                    String sourceMethodName = "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
+                    String targetMethodName = "set" + s.substring(0, 1).toUpperCase() + s.substring(1);
+                    Method sourceMethod;
+                    Method targetMethod;
+                    try {
+                        sourceMethod = kClass.getDeclaredMethod(sourceMethodName);
+                        targetMethod = vClass.getDeclaredMethod(targetMethodName, field.getType());
+                    } catch (NoSuchMethodException e) {
+                        throw new ObjectTransException("无法获取到对应get/set方法。", e);
+                    }
+                    Object invoke = sourceMethod.invoke(k);
+                    targetMethod.invoke(v, DeepCloneUtil.clone(invoke));
+                }
             }
         }
         return v;
@@ -92,7 +98,8 @@ public class ObjectTransUtil {
 
     /**
      * 兼容已过时注解，随时可能删除
-     * @param kClass 待转换对象类型
+     *
+     * @param kClass    待转换对象类型
      * @param fieldList 变量列表
      * @return 待转换变量列表
      * @see BeanTrans 已过时注解
